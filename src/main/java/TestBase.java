24793187db05b344a7bd0185eae102dcc3063ea9
vk <volodya.kotsinyan@gmail.com>
2025-11-22 22:40:38 +0400

    spribe: Few changes in TestBase

--- src/main/java/TestBase.java
+++ src/main/java/TestBase.java
@@ -1,2 +1,18 @@
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
 public abstract class TestBase {
+
+
+    @BeforeSuite
+    public void beforeSuite() {
+            //1. Read and load properties
+            //2. init logger
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+            //1. generate report
+            //2?
+    }
 }
