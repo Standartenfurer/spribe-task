659d9b2c8863f200eff458544bc38b23a33537b6
vk <volodya.kotsinyan@gmail.com>
2025-11-22 22:26:07 +0400

    spribe: added utility class for reading properties from file

---
+++ src/main/java/utils/PropertyLoader.java
@@ -0,0 +1,32 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyLoader {
+
+    private static Properties properties;
+
+    private PropertyLoader(){}
+
+    public static Properties getProperties() {
+        if (properties == null) {
+            properties = new Properties();
+            return properties;
+        }
+        return properties;
+    }
+
+    public static Properties loadProperties() {
+
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            properties.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties;
+    }
+
+
+}
